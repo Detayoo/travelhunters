ffcb37bb11c8f47f897fd5d64f59a8e2a229ee3b
Tayo <teeydigba@gmail.com>
2025-06-13 08:54:38 +0100

    added location and date filter parameters

--- services/queries.ts
+++ services/queries.ts
@@ -1,6 +1,20 @@
 import { baseApi } from "./api";
 
-export const getSearchResultsFn = async () => {
-  const { data } = await baseApi.get("/hotel/hotels");
+export const getSearchResultsFn = async ({
+  endDate,
+  location,
+  startDate,
+}: {
+  endDate?: string;
+  location?: string;
+  startDate?: string;
+}) => {
+  const params: any = {};
+  if (location) params.location = location;
+  if (startDate) params.startDate = startDate;
+  if (endDate) params.endDate = endDate;
+  const { data } = await baseApi.get("/hotel/hotels", {
+    params,
+  });
   return data;
 };
